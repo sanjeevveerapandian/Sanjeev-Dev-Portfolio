9d08d56e8ca3c3da23b892a0627d13dd07b57d06
sanjeevveerapandian <115895064+sanjeevveerapandian@users.noreply.github.com>
2025-01-01 11:43:16 +0530

    hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
 const Hero = () => {
   return (
-    <div className="pb-20 pt-36 bg-black text-white">
+    <div className="pb-20 pt-36 bg-black-100 text-white">
       {/**
        *  UI: Spotlights
        *  Link: https://ui.aceternity.com/components/spotlight
@@ -27,7 +27,7 @@ const Hero = () => {
        *  UI: Grid Background
        *  Change the background to dark grid with low opacity
        */}
-      <div className="h-screen w-full dark:bg-black-100 bg-black absolute top-0 left-0 flex items-center justify-center">
+      <div className="h-screen w-full dark:bg-black-100 bg-black-100 absolute top-0 left-0 flex items-center justify-center">
         {/* Radial gradient for dark background effect */}
         <div
           className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
@@ -50,7 +50,7 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Dynamic Web Magic with Next.js
+            Transforming Ideas into Code !
           </p>
 
           {/**
@@ -58,17 +58,17 @@ const Hero = () => {
            *  Update text size for better responsiveness in dark mode
            */}
           <TextGenerateEffect
-            words="Transforming Concepts into Seamless User Experiences"
+            words=" Hi! I'm Sanjeev , a Full-Stack Developer"
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
 
-          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Sanjeev, a Full-Stack Developer based in Chennai.
+          <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-50">
+            Passionate about writing clean, efficient code.
           </p>
 
           <a href="#about">
             <MagicButton
-              title="Show my work"
+              title="About me"
               icon={<FaLocationArrow />}
               position="right"
             />
